08d428efc3216f0fcad8f6e00edcb5a63887f2d9
Marcielle Paula <marcielle0644@gmail.com>
2025-04-10 00:53:33 -0300

    adicionando funcao para gerar slug automaticamente

--- app/Http/Controllers/Api/PlaceController.php
+++ app/Http/Controllers/Api/PlaceController.php
@@ -7,7 +7,7 @@ use App\Http\Resources\PlaceResource;
 use App\Services\PlaceService;
 use App\Traits\ApiResponse;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Str;
 
 class PlaceController extends Controller
 {
@@ -23,43 +23,42 @@ class PlaceController extends Controller
     public function index(Request $request)
     {
         $places = $this->service->list($request->all());
-        return $this->success(PlaceResource::collection($places), 'List of places');
+        return $this->success(PlaceResource::collection($places));
     }
 
     public function store(Request $request)
     {
+
+
         $data = $request->validate([
             'name' => 'required|string',
-            'slug' => 'required|string|unique:places',
             'city' => 'required|string',
             'state' => 'required|string',
         ]);
 
+        $data['slug'] = Str::slug($data['name']);
         $place = $this->service->store($data);
-        return $this->success(new PlaceResource($place), 'Place created successfully', 201);
+        return $this->success(new PlaceResource($place), 201);
     }
 
     public function show($id)
     {
         $place = $this->service->show($id);
         if (!$place) return $this->error('Local não encontrado', 404);
-        return $this->success(new PlaceResource($place), 'Place details');
+        return $this->success(new PlaceResource($place));
     }
 
     public function update(Request $request, $id)
     {
-
-
         $data = $request->validate([
             'name' => 'sometimes|string',
-            'slug' => 'sometimes|string|unique:places,slug,' . $id . ',id',
             'city' => 'sometimes|string',
             'state' => 'sometimes|string',
         ]);
 
-
+        $data['slug'] = Str::slug($data['name']);
         $place = $this->service->update($data, $id);
-        return $this->success(new PlaceResource($place), 'Place updated');
+        return $this->success(new PlaceResource($place));
     }
 
     public function destroy($id)
